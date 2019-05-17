97a4a3c36447ebe3067bbe32471dd48db59e76b7
Nikolay Ivanov <nivanov@cfeld-pcx14659-w.desy.de>
2019-05-17 15:48:28 +0200

    compression added

--- tilt_converter.py
+++ tilt_converter.py
@@ -1,8 +1,7 @@
 import numpy as np, h5py, sys, os, errno, argparse
 
 parent_path = "/asap3/petra3/gpfs/p07/2019/data/11005196"
-user_path = "/gpfs/cfel/cxi/scratch/user/nivanov"
-output_path = "hdf5/Scan_{0:d}/scan_{0:d}_data.h5"
+output_path = "../hdf5/Scan_{0:d}/scan_{0:d}_data.h5"
 scan_path = "raw/scanFrames/Scan_{0:d}"
 log_path = "raw/Scans/Scan_{0:d}.log"
 hdf5_data_path = "/entry/instrument/detector/data"
@@ -38,13 +37,13 @@ def get_data(scan_num, detector, verbose):
     return raw_data
 
 def write_data(scan_num, verbose):
-    out_path = os.path.join(user_path, output_path.format(scan_num))
+    out_path = os.path.join(os.path.dirname(__file__), output_path.format(scan_num))
     if verbose: print('Output path: %s' % out_path)
     make_output_dir(out_path)
     out_file = h5py.File(out_path, 'w', libver='latest')
     det_group = out_file.create_group('Detectors')
     for key, item in detectors.items():
-        det_group.create_dataset(str(key), data=get_data(scan_num, item, verbose))
+        det_group.create_dataset(str(key), data=get_data(scan_num, item, verbose), compression='gzip')
     out_file.close()
     if verbose: print('Done!')
 
