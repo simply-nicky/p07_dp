dc0be7321a6743a86accc26abab541b4f8379bf1
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-05-22 16:34:28 +0200

    small fixes

--- src/wrapper.py
+++ src/wrapper.py
@@ -85,7 +85,7 @@ class Scan(object):
 
     def stxm(self, Detector):
         _filenames = Detector.filenames(self.scan_num, self.verbose)
-        _worker = partial(self.chunk_sum, Detector=Detector)
+        _worker = partial(self.chunk_sum, Detector=Detector, cls=type(self))
         return utils.get_data(_filenames, _worker, self.verbose)
 
     def full_data(self):
